0494ef4704427f4a34a5e336cd0e4c579602a923
Martin-Alexander <martingianna@gmail.com>
2020-11-01 09:25:40 -0500

    chats#show and chats#index styling

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -16,7 +16,7 @@ class ChatsController < ApplicationController
 
     messages = @chat.messages.order(created_at: :asc).includes(:receiver, :sender)
     your_plant_interests = policy_scope(PlantInterest).joins(:plant).where(user: current_user).includes(:user, :plant)
-    plant_interests_of_other_user = policy_scope(PlantInterest).joins(:plant).where(plants: { user: current_user }).includes(:user, :plant)
+    plant_interests_of_other_user = policy_scope(PlantInterest).joins(:plant).where(plants: { user: current_user }, user: @chat.other_user).includes(:user, :plant)
     plant_interests = your_plant_interests.or(plant_interests_of_other_user)
 
     @messages_and_plant_interest = (messages.to_a + plant_interests.to_a).sort_by { |record| record.created_at }
