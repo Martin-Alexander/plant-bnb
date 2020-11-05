27a3745d111ac908e446d8d37e254fbb92f722a1
Martin-Alexander <martingianna@gmail.com>
2020-11-05 09:26:15 -0500

    new seed

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class Plant < ApplicationRecord
   belongs_to :user
   has_many :plant_categories, dependent: :destroy
@@ -6,44 +8,61 @@ class Plant < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :ratings, dependent: :destroy
 
+  has_many_attached :pictures
+  has_one_attached :cover_photo
+
   validates :title, presence: true, length: { maximum: 80 }
   validates :description, length: { maximum: 2000 }
 
   validate :validate_has_cover_photo
   validate :validate_has_no_more_than_four_pictures
 
-  has_many_attached :pictures
-  has_one_attached :cover_photo
-
   include PgSearch::Model
   pg_search_scope(:search_by_title_and_description,
-    against: [ :title, :description ],
-    using: {
-      tsearch: { prefix: true }
-    }
+  against: [ :title, :description ],
+  using: {
+    tsearch: { prefix: true }
+  }
   )
-
+  
   def public
     !private
   end
-
+  
   def price=(price_in_dollars)
     self.price_cents = price_in_dollars.to_i * 100
   end
-
+  
   def price
     price_cents / 100
   end
-
+  
   def validate_has_cover_photo
     if !cover_photo.attached?
       errors.add(:cover_photo, "can't be blank")
     end
   end
-
+  
   def validate_has_no_more_than_four_pictures
     if pictures.length > 3
       errors.add(:pictures, "maximum of three")
     end
   end
+
+  # Cloudinary, when requested for a never-before-requested transformed image, takes some time to make the transformation
+
+  # private
+
+  # include CloudinaryHelper
+  # after_commit :pre_generate_cloudinary_crops
+
+  # def pre_generate_cloudinary_crops
+  #   if cover_photo.attached?
+  #     URI.open(cl_image_path(cover_photo.key, height: 380, crop: :scale))
+  #   end
+
+  #   if pictures.attached?
+  #     pictures.each { |pic| URI.open(cl_image_path(pic.key, height: 380, crop: :scale)) }
+  #   end
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+srand(1)
+
 require "open-uri"
+require "csv"
 
 class ApplicationRecord
   after_save :log_create_for_seed
@@ -13,17 +16,6 @@ class ApplicationRecord
   end
 end
 
-def create_plant_from_existing_cl_file(cl_image_id, plant_attriubutes)
-  url = "https://res.cloudinary.com/dr6nzroni/image/upload/#{cl_image_id}.png"
-
-  plant = Plant.new(plant_attriubutes)
-
-  plant.cover_photo.attach(io: URI.open(url), filename: cl_image_id, content_type: "image/png")
-  plant.save!
-
-  plant
-end
-
 User.destroy_all
 Category.destroy_all
 
@@ -35,110 +27,156 @@ shrubs_category          = Category.create!(name: "shrubs")
 trees_category           = Category.create!(name: "trees")
 artificial_category      = Category.create!(name: "artificial")
 
-martin = User.new( name: "Martin", email: "martin@email.com", password: "123456")
-martin.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1603708766/plant-bnb/martin.jpg"), filename: "martin", content_type: "image/jpg")
-martin.save!
+admin = User.new(name: "Martin", email: "admin@email.com", password: "123456")
+admin.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1603708766/plant-bnb/martin.jpg"), filename: "admin", content_type: "image/jpg")
+admin.save!
 
-aline = User.new( name: "Aline", email: "aline@email.com", password: "123456")
+sangwoo = User.new(name: "Sangwoo", email: "sangwoo@email.com", password: "123456")
+sangwoo.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1603708766/plant-bnb/sangwoo.jpg"), filename: "sangwoo", content_type: "image/jpg")
+sangwoo.save!
+
+aline = User.new(name: "Aline", email: "aline@email.com", password: "123456")
 aline.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/aline.jpg"), filename: "aline", content_type: "image/jpg")
 aline.save!
 
-martins_plant_1 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-1",
-  user: martin,
-  price_cents: 900,
-  title: "Plante hypoeste rose/pink polka dots plant",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [trees_category, for_your_garden_category]
-)
+lea = User.new(name: "Lea", email: "lea@email.com", password: "123456")
+lea.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/lea.png"), filename: "lea", content_type: "image/png")
+lea.save!
 
-martins_plant_2 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-2",
-  user: martin,
-  price_cents: 800,
-  title: "Plante mauve oxalis",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [house_plants_category, artificial_category]
-)
+camille = User.new(name: "Camille", email: "camille@email.com", password: "123456")
+camille.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/camille.jpg"), filename: "camille", content_type: "image/jpg")
+camille.save!
 
-martins_plant_3 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-3",
-  user: martin,
-  price_cents: 0,
-  title: "Trinidad Moruga Scorpion bio/ Organic trinidad Moruga ",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [trees_category, shrubs_category]
-)
+stephane = User.new(name: "Stephane", email: "stephane@email.com", password: "123456")
+stephane.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/stephane.jpg"), filename: "stephane", content_type: "image/jpg")
+stephane.save!
 
-martins_plant_4 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-4",
-  user: martin,
-  price_cents: 900,
-  title: "28'' Artificial Foliage Plant",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  private: true,
-  categories: [trees_category, shrubs_category, office_plants_category]
-)
+mg = User.new(name: "MG", email: "mg@email.com", password: "123456")
+mg.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/mg.jpg"), filename: "mg", content_type: "image/jpg")
+mg.save!
 
-alines_plant_1 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-5",
-  user: aline,
-  price_cents: 900,
-  title: "Artificial Fiddle Leaf Fig Plant",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [trees_category, office_plants_category]
-)
+antoine = User.new(name: "Antoine", email: "antoine@email.com", password: "123456")
+antoine.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/antoine.jpg"), filename: "antoine", content_type: "image/jpg")
+antoine.save!
 
-alines_plant_2 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-6",
-  user: aline,
-  price_cents: 900,
-  title: "Plante araignée frisée/ curly spider plant",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [trees_category, artificial_category]
-)
+alina = User.new(name: "Alina", email: "alina@email.com", password: "123456")
+alina.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/alina.jpg"), filename: "alina", content_type: "image/jpg")
+alina.save!
 
-alines_plant_3 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-7",
-  user: aline,
-  price_cents: 900,
-  title: "Bulbes de Canna feuilles rouge",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [artificial_category, office_plants_category]
-)
+alex = User.new(name: "Alex", email: "alex@email.com", password: "123456")
+alex.profile_picture.attach(io: URI.open("https://res.cloudinary.com/dr6nzroni/image/upload/v1604297653/plant-bnb/alex.jpg"), filename: "alex", content_type: "image/jpg")
+alex.save!
 
-alines_plant_4 = create_plant_from_existing_cl_file(
-  "v1603708107/plant-bnb/plant-8",
-  user: aline,
-  price_cents: 0,
-  title: "Mother-in-law-tongue/Snake Plant/Sansevieria t",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  categories: [trees_category]
-)
+CSV.read(Rails.root.join("db/seed_data.csv"), { headers: true }).each do |row|
+  plant = Plant.new({
+    user: User.find_by_name(row["user"]),
+    price_cents: row["price"].to_i,
+    title: row["name"].send(["capitalize", "titleize", "upcase", "downcase"].sample),
+    description: "Water slowly and deeply every 2 days and keep in moderate sunlight.",
+    categories: [Category.find_by_name(row["primary_category"])]
+  })
 
-alines_plant_5 = create_plant_from_existing_cl_file(
-  "v1603708108/plant-bnb/plant-9",
-  user: aline,
-  price_cents: 1100,
-  title: "PLANTES DE FIGUES EN POT",
-  description: "Indoor evergreen plant, in different sizes. Easy to care.",
-  private: true,
-  categories: [trees_category, trees_category]
-)
+  file_extension = row["cover_photo_url"].match(/\.(\w+)$/)[1]
+  plant.cover_photo.attach(io: URI.open(row["cover_photo_url"]), filename: row["name"], content_type: "image/#{file_extension}")
+  plant.save!
+end
 
-Favourite.create!(user: martin, plant: alines_plant_1)
-Favourite.create!(user: aline, plant: martins_plant_1)
+# sangwoos_plant_1 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-1",
+#   user: sangwoo,
+#   price_cents: 900,
+#   title: "Plante hypoeste rose/pink polka dots plant",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [trees_category, for_your_garden_category]
+# )
 
-Rating.create!(user: martin, plant: alines_plant_2, stars: 5)
+# sangwoos_plant_2 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-2",
+#   user: sangwoo,
+#   price_cents: 800,
+#   title: "Plante mauve oxalis",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [house_plants_category, artificial_category]
+# )
 
-Rating.create!(user: aline, plant: martins_plant_2, stars: 5)
+# sangwoos_plant_3 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-3",
+#   user: sangwoo,
+#   price_cents: 0,
+#   title: "Trinidad Moruga Scorpion bio/ Organic trinidad Moruga ",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [trees_category, shrubs_category]
+# )
 
-PlantInterest.create!(user: aline, plant: martins_plant_2)
-message = Message.create!(sender: aline, receiver: martin, content: "Hey Martin! Can I come pick up this plant tomorrow morning?")
-alines_chat_with_martin = Chat.create!(user: aline, other_user: martin, number_of_unread_messages: 0)
-martins_chat_with_aline = Chat.create!(user: martin, other_user: aline, number_of_unread_messages: 1)
-Notification.create!(
-  chat: martins_chat_with_aline,
-  title: "<strong>#{aline.name}</strong> is interested in your plant \"<strong>#{martins_plant_2.title}</strong>\"",
-  message_preview: message.content
-)
+# sangwoos_plant_4 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-4",
+#   user: sangwoo,
+#   price_cents: 900,
+#   title: "28'' Artificial Foliage Plant",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   private: true,
+#   categories: [trees_category, shrubs_category, office_plants_category]
+# )
+
+# alines_plant_1 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-5",
+#   user: aline,
+#   price_cents: 900,
+#   title: "Artificial Fiddle Leaf Fig Plant",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [trees_category, office_plants_category]
+# )
+
+# alines_plant_2 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-6",
+#   user: aline,
+#   price_cents: 900,
+#   title: "Plante araignée frisée/ curly spider plant",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [trees_category, artificial_category]
+# )
+
+# alines_plant_3 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-7",
+#   user: aline,
+#   price_cents: 900,
+#   title: "Bulbes de Canna feuilles rouge",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [artificial_category, office_plants_category]
+# )
+
+# alines_plant_4 = create_plant_from_existing_cl_file(
+#   "v1603708107/plant-bnb/plant-8",
+#   user: aline,
+#   price_cents: 0,
+#   title: "Mother-in-law-tongue/Snake Plant/Sansevieria t",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   categories: [trees_category]
+# )
+
+# alines_plant_5 = create_plant_from_existing_cl_file(
+#   "v1603708108/plant-bnb/plant-9",
+#   user: aline,
+#   price_cents: 1100,
+#   title: "PLANTES DE FIGUES EN POT",
+#   description: "Indoor evergreen plant, in different sizes. Easy to care.",
+#   private: true,
+#   categories: [trees_category, trees_category]
+# )
+
+# Favourite.create!(user: sangwoo, plant: alines_plant_1)
+# Favourite.create!(user: aline, plant: sangwoos_plant_1)
+
+# Rating.create!(user: sangwoo, plant: alines_plant_2, stars: 5)
+
+# Rating.create!(user: aline, plant: sangwoos_plant_2, stars: 5)
+
+# PlantInterest.create!(user: aline, plant: sangwoos_plant_2)
+# message = Message.create!(sender: aline, receiver: sangwoo, content: "Hey Martin! Can I come pick up this plant tomorrow morning?")
+# alines_chat_with_sangwoo = Chat.create!(user: aline, other_user: sangwoo, number_of_unread_messages: 0)
+# sangwoos_chat_with_aline = Chat.create!(user: sangwoo, other_user: aline, number_of_unread_messages: 1)
+# Notification.create!(
+#   chat: sangwoos_chat_with_aline,
+#   title: "<strong>#{aline.name}</strong> is interested in your plant \"<strong>#{sangwoos_plant_2.title}</strong>\"",
+#   message_preview: message.content
+# )
