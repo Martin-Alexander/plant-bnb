c2485bfcd91ccd59903d52bd32cb327dcb0b2aee
Martin-Alexander <martingianna@gmail.com>
2020-11-06 09:31:50 -0500

    fixd db n+1 isue

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,14 @@ class CategoriesController < ApplicationController
 
     @category = Category.find(params[:id])
 
-    @plants = policy_scope(Plant).joins(plant_categories: :category).where(categories: { id: @category.id }).includes(:user)
+    @plants = policy_scope(Plant)
+      .joins(plant_categories: :category)
+      .where(categories: { id: @category.id })
+      .order("user_id = #{current_user&.id || 0}")
+      .includes([
+        { user: { profile_picture_attachment: :blob }},
+        { favourites: :user }
+      ])
+      .with_attached_cover_photo
   end
 end
